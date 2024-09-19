3d24946efde8e5cd423a1159237a0d15cfb5139f
dell <dell@DESKTOP-AG8UVHL>
2024-09-19 23:29:04 +0530

    First commit

---
+++ EmployeeMVC/src/main/java/com/example/kartiksapra/mvcproj/services/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.example.kartiksapra.mvcproj.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+	//create
+	//all employee read
+	//update
+	
+}
